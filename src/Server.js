ad9b0f9ba20af1e793983723bc67b06e62b69e3e
govind-anjana <govindanjana2004@gmail.com>
2025-11-13 17:45:17 +0530

    new edit

--- src/Server.js
+++ src/Server.js
@@ -1,32 +1,22 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import connectDB from '../src/config/db.js'
 import cors from "cors";
-import { createContact, getContacts } from "./Controller/contactController.js";
+import contactRoutes from '../src/routes/contactRoutes.js'
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+const PORT = process.env.PORT || 5000;
 
 // MongoDB Connection
-mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => console.log("✅ MongoDB connected"))
-  .catch((err) => console.log("❌ MongoDB connection error:", err));
+connectDB();
 
 
-//  Create Contact
-app.post("/contact", createContact);
-
-// 📄 Get All Contacts (Admin Only)
-app.get("/contact",getContacts)
-// Default route
-app.get("/", (req, res) => {
-  res.send("🚀 Server is running successfully!");
-});
+app.use("/contact",contactRoutes);
 
 // Start server
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
