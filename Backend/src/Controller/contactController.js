c6eb38df05b61e211d333561912402c5b1d8b791
govind-anjana <govindanjana2004@gmail.com>
2025-11-13 13:58:04 +0530

    first commit

---
+++ Backend/src/Controller/contactController.js
@@ -0,0 +1,64 @@
+import nodemailer from "nodemailer";
+import dotenv from 'dotenv';
+import ContactModel from "../model/ContactModel.js";
+
+dotenv.config();
+//  Create new contact (POST)
+export const createContact = async (req, res) => {
+  try {
+    const { username, email, phone, message } = req.body;
+
+    // Gmail transporter
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER, // Admin Gmail
+        pass: process.env.EMAIL_PASS,  // App Password
+      },
+    });
+
+    // Mail content
+    const mailOptions = {
+      from: email, // user's email
+      to: process.env.EMAIL_USER, // admin's email
+      subject: `New contact from ${username}`,
+      text: `
+        Username: ${username}
+        Email: ${email}
+        Phone: ${phone}
+        Message: ${message}
+      `,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    res.status(200).json({
+      success: true,
+      message: "Email sent successfully to admin!",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to send email",
+      error: error.message,
+    });
+  }
+};
+
+//  Get all contacts (GET)
+export const getContacts = async (req, res) => {
+  try {
+    const contacts = await ContactModel.find().sort({ createdAt: -1 });
+    res.status(200).json({
+      success: true,
+      count: contacts.length,
+      data: contacts,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error fetching contacts",
+      error: error.message,
+    });
+  }
+};
